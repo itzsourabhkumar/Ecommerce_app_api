916ea05cfb10ff33686d460acd64a5f3a6f7b839
itzsourabh <85216659+itzsourabhkumar@users.noreply.github.com>
2023-10-10 11:25:32 +0530

    Feature: Added product model using sequelize cli

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -3,32 +3,32 @@ const categoryValidator = require("../middlewares/category.validator");
 
 const routes = (app) => {
   app.post(
-    "/ecom/api/v1/category",
+    "/ecom/api/v1/categories",
     categoryValidator.validateCreate,
     categoryController.createCategory
   );
 
-  app.get("/ecom/api/v1/category", categoryController.getAllCategories);
+  app.get("/ecom/api/v1/categories", categoryController.getAllCategories);
 
   app.get(
-    "/ecom/api/v1/category/:id",
+    "/ecom/api/v1/categories/:id",
     categoryValidator.validateGetById,
     categoryController.getCategoryById
   );
 
   app.put(
-    "/ecom/api/v1/category/:id",
+    "/ecom/api/v1/categories/:id",
     categoryValidator.validateUpdate,
     categoryController.updateCategory
   );
 
   app.patch(
-    "/ecom/api/v1/category/:id",
+    "/ecom/api/v1/categories/:id",
     categoryValidator.validatePartialUpdate,
     categoryController.updateCategory
   );
 
-  app.delete("/ecom/api/v1/category/:id", categoryController.deleteCategory);
+  app.delete("/ecom/api/v1/categories/:id", categoryController.deleteCategory);
 };
 
 module.exports = routes;
