35398600b49be469835764b0d902f97842ad262e
itzsourabh <85216659+itzsourabhkumar@users.noreply.github.com>
2023-10-10 14:59:32 +0530

    Feature: Added create product API

---
+++ middlewares/product.validator.js
@@ -0,0 +1,41 @@
+const productCreateValidator = (req, res, next) => {
+  if (!req.body.name || !req.body.cost) {
+    return res.status(400).json({
+      message: "Invalid request body",
+      success: false,
+      data: {},
+      err: "Either name or cost or category is missing from the request object",
+    });
+  }
+  next();
+};
+
+const productUpdateValidater = (req, res, next) => {
+  if (!req.body.name || !req.body.description || !req.body.cost) {
+    return res.status(400).json({
+      message: "Invalid request body",
+      success: false,
+      data: {},
+      err: "Either name, description or cost is missing from the request object",
+    });
+  }
+  next();
+};
+
+const productPatchValidator = (req, res, next) => {
+  if (!(req.body.name || req.body.description || req.body.cost)) {
+    return res.status(400).json({
+      message: "Invalid request body",
+      success: false,
+      data: {},
+      err: "At least one parameter among cost, name and description is expected",
+    });
+  }
+  next();
+};
+
+module.exports = {
+  productCreateValidator,
+  productUpdateValidater,
+  productPatchValidator,
+};
